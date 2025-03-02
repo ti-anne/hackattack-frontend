85a13d44e4ad3b806fa77a2e9d3a913f7ad12643
Moop333 <Moop3334@gmail.com>
2025-03-01 17:50:57 -0800

    Basic Navbar functionality (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
 import './App.css';
+import './Navbar.css';
 import gam_nonfocus from './assets/gam_nonfocus.png';
 import gam_focus from './assets/gam_focus.png';
 import Report from './Report'; 
+
+import './fonts/OpenDyslexic-Regular.woff'
+import './fonts/OpenDyslexic-Italic.woff'
+import './fonts/OpenDyslexic-Bold.woff'
+import './fonts/OpenDyslexic-Bold-Italic.woff'
 
 function App() {
   const [isInputFocused, setIsInputFocused] = useState(false);
   const [inputValue, setInputValue] = useState(''); 
   const [showReport, setShowReport] = useState(false); 
   const [submittedUrl, setSubmittedUrl] = useState(''); 
+  const [login, setLogin] = useState(true);
+
+  const handleUser = () => {
+    if (!login) {
+      setLogin(true);
+    } else {
+      setLogin(false);
+    }
+  }
 
   const handleInputFocus = () => {
     setIsInputFocused(true);
@@ -41,6 +56,27 @@ function App() {
 
   return (
     <>
+    <header>
+    <nav className="navbar">
+      <ul className='navbar'>
+        <li class='navitem left'><a href='/'>Home</a></li>
+        <li class='navitem left'><a href='/tutorials'>Tutorials</a></li>
+        <li class='navitem left'><a href='/resources'>Resources</a></li>
+        {login ? (
+          <>
+            <li class='navitem right user'><a href="#" onClick={handleUser}>Log Out</a></li>
+            <li class='navitem right user'><a href='/Report'>Reports</a></li>
+          </>
+        ) : (
+          <>
+            <li class='navitem right sign'><a href='/signUp'>Sign Up</a></li>
+            <li class='navitem right login'><a href="#" onClick={handleUser}>Login</a></li>
+          </>
+        )}
+        <li class='navitem right'>Darkmode</li>
+      </ul>  
+    </nav>
+    </header>
     <div className="container">
       <div className='search-container'>
         <img
