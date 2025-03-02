afa1f3d62c3143aca0415ceb0247ea82de03d8b1
An Tran <atran10@student.ubc.ca>
2025-03-01 20:35:02 -0800

    added scrolling animation with gif

--- src/Report.jsx
+++ src/Report.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './Report.css';
 import bad_score from './assets/bad_score.png';
 import good_score from './assets/good_score.png';
+import studious from './assets/studious.png';
+import splaptop from './assets/splaptop.gif';
 
 const Report = ({ url }) => {
   const [score, setScore] = useState(-1);
@@ -17,21 +19,33 @@ const Report = ({ url }) => {
 
       <h1>Report for {url}</h1>
       <div className='overall'>
-      <img
-        src={score > 50 ? good_score : bad_score}
-        alt="gamgam telling how accessible your site is"
-        className="gam-reaction"
-        onClick={imageClick}
-      />
-      <div className='comment'>
-        <h2>You scored XYZ</h2>
-        <p>You passed x tests and failed y tests.</p>
+        <img
+          src={score > 50 ? good_score : bad_score}
+          alt="gamgam telling how accessible your site is"
+          className="gam-reaction"
+          onClick={imageClick}
+        />
+        <div className='comment'>
+          <h2>You scored XYZ</h2>
+          <p>You passed x tests and failed y tests.</p>
+        </div>
       </div>
-      <div className="">
-
+      <div className='overall'>
+        <div className='comment'>
+        <h2>Your assigned readings</h2>
+        <ul>
+          <li><a href='https://www.w3.org/WAI/fundamentals/accessibility-intro/'>Accessibility Fundamentals</a></li>
+          <li><a href='https://www.w3.org/WAI/WCAG22/quickref/'>Accessibility Thorough Guide  </a></li>
+        </ul>
+        </div>
+        <img
+          src={score > 50 ? studious : splaptop}
+          alt="gamgam telling how accessible your site is"
+          className="gam-reaction"
+          onClick={imageClick}
+        />
       </div>
       
-      </div>
       
     </div>
   );
